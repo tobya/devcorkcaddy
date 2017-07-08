b10cade300e34f674419875b985ad58d5eeb9ccf
tobya <tobyallen@toflidium.com>
2017-07-08 15:43:28 +0000

    changes to filese

---
+++ allsite/index.php
@@ -0,0 +1,18 @@
+<?php
+
+	$SiteName = $_SERVER['HTTP_HOST'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to <?php echo $SiteName; ?></title>
+</head>
+<body>
+<h2>
+Welcome to your secure <?php echo $SiteName; ?>
+</h2>
+</body>
+</html>
+
